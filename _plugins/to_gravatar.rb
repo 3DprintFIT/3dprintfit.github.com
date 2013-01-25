33077c462710321f725787078fa111a2b38dd2c5
Miro Hrončok <miro@hroncok.cz>
2013-01-25 16:59:24 +0100

    Test

--- _plugins/to_gravatar.rb
+++ _plugins/to_gravatar.rb
@@ -3,7 +3,7 @@ require 'digest/md5'
 module Jekyll
   module GravatarFilter
     def to_gravatar(input)
-      "https://www.gravatar.com/avatar/#{hash(input)}?size=100&d=identicon&.png"
+      "https://www.gravatar.com/avatar/#{hash(input)}"
     end
 
     private :hash
